13d21bb5e19ef25ddbe0c7ff25f206f7ef5ed612
Alexey Popov <flyslam@yandex.ru>
2019-11-10 19:44:11 +0300

    Added new entity Training

---
+++ src/Controller/TrainingController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Training;
+use App\Form\Type\TrainingType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrainingController extends AbstractController
+{
+    /**
+     * @Route("/training/all", name="training_list")
+     */
+    public function index()
+    {
+        $allTraining = $this->getDoctrine()->getRepository(Training::class)
+            ->findAll();
+
+        return $this->render('training/index.html.twig', [
+            'trainingAll' => $allTraining,
+        ]);
+    }
+
+    /**
+     * @Route("/training/create", name="training_create")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function create(Request $request)
+    {
+        $training = new Training();
+
+        $form = $this->createForm(TrainingType::class, $training);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $training = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($training);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('training_list');
+        }
+
+        return $this->render('training/create.html.twig', [
+            'form' => $form->createView(),
+            'error' => $form->getErrors()
+        ]);
+    }
+
+    /**
+     * @Route("/training/{id}", name="training_show", methods={"GET"}, requirements={"id"="\d+"})
+     *
+     * @ParamConverter("training", class="App:Training")
+     * @param Training $training
+     *
+     * @return Response
+     */
+    public function show(Training $training)
+    {
+        if (!$training) {
+            throw $this->createNotFoundException(
+                'No training found for id ' . $training->getId()
+            );
+        }
+
+        return $this->render('training/show.html.twig', [
+            'training' => $training,
+        ]);
+    }
+
+    /**
+     * @Route("/training/{id}/detele", name="training_delete", requirements={"id"="\d+"})
+     *
+     * @ParamConverter("training", class="App:Training")
+     * @param Training $training
+     *
+     * @return RedirectResponse
+     */
+    public function delete(Training $training)
+    {
+        if (!$training) {
+            throw $this->createNotFoundException(
+                'No job found for id ' . $training->getId()
+            );
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($training);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('training_list', [], 302);
+    }
+}
